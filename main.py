2d085148c188b6c4c438be7ca2724c40c7bd60b6
rafaelreis8000 <priv.rafa.reis@gmail.com>
2025-04-16 00:52:53 -0300

    finalização da inserção do valor da aposta
    o usuário começa com 20 reais na conta, que pode aumentar ou perder

--- main.py
+++ main.py
@@ -50,11 +50,12 @@ while jogo == False:
             print('{}\n \033[31mInsira um valor válido\n   e tente novamente!\033[0m'.format('='*25))
             sleep(2)
 
+
 menu_inicial = bool(True)
 while menu_inicial == True:
     system('cls')
     sleep(0.2)
-    saldo = int(20)
+    saldo = float(20)
     
     escolha_menu = input(
         '{}\n\033[33m    BET DOS BICHINHOS\033[0m'
@@ -64,12 +65,51 @@ while menu_inicial == True:
         .format('='*25)
     ).strip().upper()
 
-    if escolha_menu == '1':
+    if escolha_menu == '1': #encerra o menu inicial, caminhando para a tela de apostas
         menu_inicial = False
 
-    elif escolha_menu == '2':
+    elif escolha_menu == '2': #exibe o saldo em conta
         
         system('cls')
         sleep(0.2)
-        print('{}\n  \033[33mVocê tem um saldo de:\033[0m\n\n        \033[32mR${},00\033[0m\n\n{}'.format('='*25 , saldo , '='*25))
+        print('{}\n  \033[33mVocê tem um saldo de:\033[0m\n\n        \033[32mR${:.2f}\033[0m\n\n{}'.format('='*25 , saldo , '='*25))
         sleep(2)
+
+tela_aposta1 = bool(True)
+while tela_aposta1 == True:
+    system('cls')
+    sleep(0.2)
+
+    try:
+        valor_aposta = float(
+            input(
+                '{}\n\033[33mInforme o valor da aposta\033[0m'
+                '\n\nSeu saldo é de \033[32mR${:.2f}\033[0m'
+                '\n\n\033[31mapostas mínimas de 1 rea\033[0m'
+                '\n\n \033[35m-->\033[0m  '
+                .format('='*25 , saldo)
+            )
+        )
+
+        if valor_aposta > saldo: #se o valor exceder o saldo
+            system('cls')
+            sleep(0.2)
+            print('{}\n \033[31mInsira um valor válido\n   e tente novamente!\033[0m'.format('='*25))
+            sleep(2)
+
+        elif valor_aposta < 1: #se a van for menor que 1
+            system('cls')
+            sleep(0.2)
+            print('{}\n   \033[31mA aposta não pode\n ser menor que 1 real!\033[0m'.format('='*25))
+            sleep(2)
+
+        else: #se o valor estiver dentro de saldo, encerra a tela de apostas 1
+            tela_aposta1 = False
+    
+    except ValueError: #em caso de erro de digitação, caracteres especiais...
+        system('cls')
+        sleep(0.2)
+        print('{}\n \033[31mInsira um valor válido\n   e tente novamente!\033[0m'.format('='*25))
+        sleep(2)
+
+print('Jogo do tigrinho')
